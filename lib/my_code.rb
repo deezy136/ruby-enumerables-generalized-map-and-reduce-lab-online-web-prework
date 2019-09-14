c4e9de076a716080cb5b67a3c5b1c3e5a1aa8b5e
deezy13 <deezy13@gmail.com>
2019-09-14 19:46:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,40 +1,7 @@
-  def map(source_array)
-    p source_array
-     new_arr =[]
-     if block_given?
-    i = 0
-    p "before yield is ran"
-    while i < source_array.length
-     new_arr << yield(source_array[i])
-           i = i + 1
-    end
-    p "inside method after yield"
-
-    new_arr
-  else
-    puts "Hey! No block was given!"
-  end
-end
-
-
-def reduce(source_array, starting_point=nil) 
-     if starting_point
-         total = starting_point
-         counter = 0
-    else
-      total = source_array[0]
-      counter = 1
-    end
+  def map(array)
     
-   while counter < source_array.size do
-    yield(total, source_array[counter])
-     counter +=1
- end
-   total
- end 
-
- reduce([1,2,3,4]) { |x , m| x + m}
-
-
-
-
+  end
+  
+  def reduce(array, sp =nil)
+    
+  end
